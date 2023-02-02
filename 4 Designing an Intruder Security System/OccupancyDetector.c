9153c8830160a282e1c599d2c7ab37a6de07f6a2
odonne36 <odonne36@students.rowan.edu>
2023-02-02 18:06:40 -0500

    started part 4

--- 4 Designing an Intruder Security System/OccupancyDetector.c
+++ 4 Designing an Intruder Security System/OccupancyDetector.c
@@ -1,10 +1,27 @@
-/*
- * OccupancyDetector.c
- *
- *  Created on: Jan 30, 2023
- *      Author: russty
- */
+//Matt O'Donnell
+//2/2/23
+
+
+#include <msp430.h>
+
+ WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+ P6DIR |= BIT6;              // Configure P6.6 to an Output
+     P2DIR &= ~BIT3;             // Configure P2.3 to an Input
+
+     P2REN |= BIT3;               // Enable Resistor on P2.3
+     P2OUT |= BIT3;               // Configure Resistor on P2.3 to be Pullup
+
+          P4DIR &= ~BIT1;
+          P4REN |= BIT1;
+          P4OUT |= BIT1;
+
+     PM5CTL0 &= ~LOCKLPM5;
+
+void main() {
+
+while (1)
 
 
 
 
+}
