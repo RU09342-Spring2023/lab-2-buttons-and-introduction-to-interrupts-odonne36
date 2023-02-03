6d4735ba4cb00ac2a9cb4574495d3ee347748d8d
odonne36 <odonne36@students.rowan.edu>
2023-02-03 15:26:54 -0500

    First working version of part 4 all functions

--- 4 Designing an Intruder Security System/OccupancyDetector.c
+++ 4 Designing an Intruder Security System/OccupancyDetector.c
@@ -7,7 +7,9 @@
 
 
 unsigned int state = 0;
-
+unsigned int reset = 0;
+unsigned int count =0;
+unsigned int detected=0;
 void main() {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
      P6DIR |= BIT6;              // Configure green led to an Output
@@ -17,7 +19,7 @@ void main() {
                    P2OUT |= BIT3;
                    P2IES &= ~BIT3;
                    P2IE |= BIT3;
-                   P2IFG &= ~BIT3;               // Configure Resistor on P2.3 to be Pullup
+                   P2IFG &= ~BIT3;
 
 
               P4REN |= BIT1;
@@ -36,28 +38,46 @@ void main() {
          __bis_SR_register(GIE);
 
 while(1) {
+
+
 while (state ==0){
     P6OUT ^= BIT6;
+    P1OUT &= ~BIT0;
     __delay_cycles(1000000);
-
+    if (detected)
+        state =1;
 }
 
-if (state == 1 ) {
-P6OUT &= ~BIT6;
 
+
+
+if (detected){
 while(state == 1) {
+    P6OUT &= ~BIT6;
     P1OUT ^= BIT0;
     __delay_cycles(100000);
-}
+    count++;
+
+    if (count == 100 && !(P4IN & BIT1)) {
+        state = 2;
+        count = 0;
+
+    }
+    else if (count == 100 && (P4IN & BIT1)){
+        state = 0;
+        count = 0;
+        detected=0;
+    }
 }
 
 
-if (state == 2) {
+}
+
+while (state == 2) {
  P1OUT |= BIT0;
  P6OUT &= ~BIT6;
 
- if (P2OUT & BIT3)
-     state=0;
+
 }
 
 
@@ -73,15 +93,19 @@ if (state == 2) {
 __interrupt void Port_4(void)
 {
 
-    P4IFG &= ~BIT1;                         // Clear P1.3 IFG
-    state = 0x01;
+    P4IFG &= ~BIT1;
+    detected = 0x01;
     P6OUT &= ~BIT6;
+
 }
 
 #pragma vector=PORT2_VECTOR
 __interrupt void Port_2(void)
 {
     P1OUT &= ~BIT0;
-    P2IFG &= ~BIT3;                         // Clear P1.3 IFG
-    state = 0x00;
+    P2IFG &= ~BIT3;
+    reset = 1;
+    count = 0;
+    detected=0;
+    state=0;
 }
