a77976553d00396adb46047279118e5faf315bdb
odonne36 <odonne36@students.rowan.edu>
2023-02-02 19:54:32 -0500

    experimental code for part 4

--- 4 Designing an Intruder Security System/OccupancyDetector.c
+++ 4 Designing an Intruder Security System/OccupancyDetector.c
@@ -5,7 +5,8 @@
 #include <msp430.h>
 
  WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
- P6DIR |= BIT6;              // Configure P6.6 to an Output
+ P6DIR |= BIT6;              // Configure green led to an Output
+ P6OUT &= ~BIT6;
      P2DIR &= ~BIT3;             // Configure P2.3 to an Input
 
      P2REN |= BIT3;               // Enable Resistor on P2.3
@@ -15,13 +16,62 @@
           P4REN |= BIT1;
           P4OUT |= BIT1;
 
+          P1DIR |= BIT0;              // Configure red led to an Output
+           P1OUT &= ~BIT0;
+
      PM5CTL0 &= ~LOCKLPM5;
 
+     unsigned int state = 0;
 void main() {
+while(1)
+while (state ==0){
+    P6OUT ^= BIT6;
+    __delay_cycles(1000000);
 
-while (1)
+}
+
+if (persondetected && state ==0x00) {
+state =1;
+P6OUT &= ~BIT6;
+
+while(persondetected()) {
+    P1OUT ^= BIT0;
+    __delay_cycles(100000);
+}
+}
+
+
+if (state == 2) {
+ P1OUT |= BIT0;
+ P6OUT &= ~BIT6;
+
+ if (P2OUT & BIT3)
+     state=0;
+}
 
 
 
 
+
+
+
+}
+}
+
+int persondetected(int x) {
+    if (!(P4OUT & BIT1 >0) && x==0) {
+        return 1;
+        __delay_cycles(10000000);
+
+        persondected(1);
+    }
+    else
+        return 0;
+
+    if (x == 1 & P4OUT & BIT1 >0)
+        persondetected(0);
+        else{
+           state=2;
+    break;
+    }
 }
